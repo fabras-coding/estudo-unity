a2c287a5e9ac567a1e40835dff2016e37ed1644a
libelola <fabriciod08@gmail.com>
2024-01-29 13:31:46 -0300

    tentando fazer o isGrounded e o .move funcionar

--- Assets/Scripts/TankWarrior.cs
+++ Assets/Scripts/TankWarrior.cs
@@ -16,70 +16,37 @@ public class TankWarrior : MonoBehaviour
 	public Animator animator = null;
 
 	//Update is called once per frame
-	//void Update()
-	//{
-
-	//	//Moving foward
-
-	//	if (characterController.isGrounded)
-	//	{
-	//		walking = new Vector3(0, 0, Input.GetAxis("Vertical"));
-	//		walking = transform.TransformDirection(walking); // Transform Direction movimenta o personagem em relação ao mundo
-	//		walking *= walkSpeed;
-	//	}
-	//	//walking.y -= gravity * Time.deltaTime;
-	//	characterController.Move(walking * Time.deltaTime);
-
-	//	//Rotate Char
-
-	//	float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
-	//	rotation *= Time.deltaTime;
-
-	//	transform.Rotate(0, rotation, 0);
-
-	//	if (characterController.isGrounded)
-	//	{
-	//		if(walking.z != 0)
-	//		{
-	//			animator.SetBool("parado", false);
-	//			animator.SetBool("andando", true);
-	//		}
-	//		else
-	//		{
-	//			animator.SetBool("parado", true);
-	//			animator.SetBool("andando", false);
-	//		}
-	//	}
-
-
-	//}
-
-
-	public float moveSpeed = 5.0f;
-	
-
-	void Update() // Funciona kkk
+	void Update()
 	{
 
 		//Moving foward
 
+		//walking.y -= gravity * Time.deltaTime;
+		//if (characterController.isGrounded) // nao funciona, ver o pq.
 
-		float move = Input.GetAxis("Vertical") * moveSpeed;
-		float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+		print("Toquei o chao");
+		walking = new Vector3(0, 0, Input.GetAxis("Vertical"));
+		walking = transform.TransformDirection(walking); // Transform Direction movimenta o personagem em relação ao mundo
+		walking *= walkSpeed;
 
-		move *= Time.deltaTime;
-		rotation *= Time.deltaTime;
 
-		
+		print("eixo Y agora: " + walking.y);
+		characterController.Move(walking * Time.deltaTime);
 
 		//Rotate Char
 
+		float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+		rotation *= Time.deltaTime;
+		print("rotation: " + rotation);
 		transform.Rotate(0, rotation, 0);
-		transform.Translate(0, 0, move);
 
-		//Animação
-		if (Input.GetKey(KeyCode.W)) //gambi kk
+
+		//Animation 
+		//if (characterController.isGrounded)// n funciona
+
+		if (walking.z != 0)
 		{
+			print("Valor do walking Z: " + walking.z);
 			animator.SetBool("parado", false);
 			animator.SetBool("andando", true);
 		}
@@ -91,11 +58,50 @@ public class TankWarrior : MonoBehaviour
 
 
 
-	}
+		//}
 
 
-	private void LateUpdate()
-	{
-		// colocar a camera aqui com position = posicao da camera em relação ao player
+		//public float moveSpeed = 5.0f;
+
+
+		//void Update() // Funciona kkk
+		//{
+
+		//	//Moving foward
+
+
+		//	float move = Input.GetAxis("Vertical") * moveSpeed;
+		//	float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+		//	move *= Time.deltaTime;
+		//	rotation *= Time.deltaTime;
+
+
+
+		//	//Rotate Char
+
+		//	transform.Rotate(0, rotation, 0);
+		//	transform.Translate(0, 0, move);
+
+		//	//Animação
+		//	if (Input.GetKey(KeyCode.W)) //gambi kk
+		//	{
+		//		animator.SetBool("parado", false);
+		//		animator.SetBool("andando", true);
+		//	}
+		//	else
+		//	{
+		//		animator.SetBool("parado", true);
+		//		animator.SetBool("andando", false);
+		//	}
+
+
+
+		}
+
+
+		private void LateUpdate()
+		{
+			// colocar a camera aqui com position = posicao da camera em relação ao player
+		}
 	}
-}
